ee46767b9c71289b768e2274bc80dabd00c54b86
AqingCyan <aqingcyan@cyanthing.com>
2021-07-25 02:37:20 +0800

    feat: make jwt token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [UserModule], // 要使用别的模块的内容，需要在这里导入
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secretOrPrivateKey: '0Qb98Vh70wfvieE5DcYqAwbfhYcZRKDy',
+      signOptions: {
+        expiresIn: '12h',
+      },
+    }),
+  ], // 要使用别的模块的内容，需要在这里导入
   providers: [AuthService],
   controllers: [AuthController],
 })
