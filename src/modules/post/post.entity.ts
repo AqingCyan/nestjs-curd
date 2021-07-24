669eb62bfbea1a11e9c3420d0610279f776d18dd
AqingCyan <aqingcyan@cyanthing.com>
2021-07-24 19:53:40 +0800

    add: connect mysql & finish data store

---
+++ src/modules/post/post.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('longtext')
+  body: string;
+
+  @CreateDateColumn()
+  created: Date;
+
+  @UpdateDateColumn()
+  updated: Date;
+}
