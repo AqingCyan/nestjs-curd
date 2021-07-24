8af9c45292bda58a5b73c48f2eea58a2355e3e6c
AqingCyan <aqingcyan@cyanthing.com>
2021-07-25 01:09:15 +0800

    feat: update password

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,5 +1,6 @@
 import {
   BeforeInsert,
+  BeforeUpdate,
   Column,
   CreateDateColumn,
   Entity,
@@ -29,7 +30,13 @@ export class User {
 
   // 监听插入实体的数据，做密码加密处理
   @BeforeInsert()
+  @BeforeUpdate()
   async hashPassword() {
     this.password = await bcrypt.hash(this.password, 12);
+  }
+
+  // 比对密码
+  async comparePassword(password: string) {
+    return await bcrypt.compare(password, this.password);
   }
 }
