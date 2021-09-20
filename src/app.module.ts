0e1f23acda19436efcb5fe912f2833fc90f7f5c6
AqingCyan <aqingcyan@cyanthing.com>
2021-09-20 16:42:12 +0800

    chore: 重新开始编写练手项目

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostModule } from './modules/post/post.module';
-import { UserModule } from './modules/user/user.module';
-import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'nest',
-      password: 'password',
-      database: 'nest',
-      synchronize: true,
-      logging: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    }),
-    PostModule,
-    UserModule,
-    AuthModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
