f788c4e330b182ee8d46ebca3720ca247cb81ec3
AqingCyan <aqingcyan@cyanthing.com>
2021-09-20 19:59:27 +0800

    feat: learn about Guard

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -5,13 +5,17 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreatePostDto } from './post.dto';
 import { DemoService } from './providers/demo/demo.service';
+import { DemoAuthGuard } from '../../core/guards/demo-auth.guard';
+import { Roles } from '../../core/decorators/roles.decorator';
 
 @Controller('posts')
+@UseGuards(DemoAuthGuard)
 export class PostsController {
   constructor(private readonly demoService: DemoService) {}
 
@@ -28,6 +32,7 @@ export class PostsController {
 
   @Post()
   @UsePipes(ValidationPipe)
+  @Roles('member')
   store(@Body() post: CreatePostDto) {
     this.demoService.create(post);
   }
