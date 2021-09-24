bf167f5a91ddf329e5a79121a5cccc9635bbe836
AqingCyan <aqingcyan@cyanthing.com>
2021-09-24 21:16:13 +0800

    core: restart demo project

--- src/core/pipes/demo.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
-@Injectable()
-export class DemoPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    console.log(value, metadata);
-    return value;
-  }
-}
