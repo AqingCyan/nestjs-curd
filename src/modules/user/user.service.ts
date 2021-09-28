cbcb7e6331c31985a80b77675ade0ebceba49bb0
AqingCyan <aqingcyan@cyanthing.com>
2021-09-28 22:39:57 +0800

    feat: exception

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -11,6 +11,13 @@ export class UserService {
   ) {}
 
   async store(data: UserDto) {
+    const { name } = data;
+    const user = await this.userRepository.findOne({ name });
+
+    if (user) {
+      throw new BadRequestException('用户已经存在');
+    }
+
     const entity = await this.userRepository.create(data);
     await this.userRepository.save(entity);
     return entity;
