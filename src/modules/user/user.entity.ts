399672f869501c24712268d83073823d907f46a9
AqingCyan <aqingcyan@cyanthing.com>
2021-09-28 22:37:08 +0800

    feat: create user

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  /* 用户ID */
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  /* 用户姓名 */
+  @Column()
+  name: string;
+
+  /* 用户密码 */
+  @Column('longtext', { nullable: true })
+  password: string;
+
+  /* 用户创建时间 */
+  @CreateDateColumn()
+  created: Date;
+
+  /* 用户更新时间 */
+  @UpdateDateColumn()
+  updated: Date;
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { UserDto } from './user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async store(data: UserDto) {
+    const entity = await this.userRepository.create(data);
+    await this.userRepository.save(entity);
+    return entity;
+  }
+}
