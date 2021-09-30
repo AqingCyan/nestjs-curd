f216fe4b61537d4cd9e218076fb6ac618b6d2456
AqingCyan <aqingcyan@cyanthing.com>
2021-09-30 23:02:15 +0800

    feat: sign token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secretOrPrivateKey: 'wRSNfNrRHEefdN84U7LdTkpg6SwZxjFW',
+      signOptions: {
+        expiresIn: '12h',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
