7a8bcb118fd231893d49561f9540c1a5fa9988ce
AqingCyan <aqingcyan@cyanthing.com>
2021-10-16 20:42:53 +0800

    feat: create role

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { RoleService } from './role.service';
+import { RoleDto } from './role.dto';
 
-@Controller('role')
-export class RoleController {}
+@Controller('roles')
+export class RoleController {
+  constructor(private readonly roleService: RoleService) {}
+
+  @Post()
+  async store(@Body() data: RoleDto) {
+    return await this.roleService.store(data);
+  }
+}
