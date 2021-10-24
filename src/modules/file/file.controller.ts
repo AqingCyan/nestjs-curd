8dc167f8bd06936f8fedd432455312cf1e0ffa17
AqingCyan <aqingcyan@cyanthing.com>
2021-10-25 01:59:10 +0800

    feat: get file

--- src/modules/file/file.controller.ts
+++ src/modules/file/file.controller.ts
@@ -1,10 +1,15 @@
 import {
   Controller,
+  Get,
+  Param,
+  ParseIntPipe,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { FileService } from './file.service';
 import { FileDto } from './file.dto';
 
@@ -16,5 +21,17 @@ export class FileController {
   @UseInterceptors(FileInterceptor('file'))
   async store(@UploadedFile() data: FileDto) {
     return await this.fileService.store(data);
+  }
+
+  @Get(':id')
+  async show(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+    const file = await this.fileService.show(id);
+
+    res.sendFile(file.filename, {
+      root: 'uploads',
+      headers: {
+        'Content-type': file.mimetype,
+      },
+    });
   }
 }
