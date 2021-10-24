f83df49d93bd44681922b9b0c7e04f6688f689dc
AqingCyan <aqingcyan@cyanthing.com>
2021-10-25 01:02:14 +0800

    feat: use guard to check user can update ? (he should be owner and vip)

--- src/core/guards/access.guard.ts
+++ src/core/guards/access.guard.ts
@@ -2,21 +2,39 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { PermissionInterface } from '../interfaces/permission.interface';
 import { User } from '../../modules/user/user.entity';
+import { Possession } from '../enums/possession.enum';
+import { UserService } from '../../modules/user/user.service';
 
 @Injectable()
 export class AccessGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly reflector: Reflector,
+  ) {}
 
-  async validatePermissions(permissions: PermissionInterface[], user: User) {
+  async validatePermissions(
+    permissions: PermissionInterface[],
+    user: User,
+    resourceId: number,
+  ) {
     const results = permissions.map(async (permission) => {
-      const { role } = permission;
-      let hasRole: boolean;
+      const { role, resource, possession } = permission;
+      let hasRole = true;
+      let hasPossession = true;
+
+      if (possession === Possession.OWN) {
+        hasPossession = await this.userService.possess(
+          user.id,
+          resource,
+          resourceId,
+        );
+      }
 
       if (role) {
         hasRole = user.roles.some((userRole) => userRole.name === role);
       }
 
-      return hasRole;
+      return hasRole && hasPossession;
     });
 
     return Promise.all(results);
@@ -26,7 +44,11 @@ export class AccessGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const permissions = this.reflector.get('permissions', context.getHandler());
 
-    const results = await this.validatePermissions(permissions, request.user);
+    const results = await this.validatePermissions(
+      permissions,
+      request.user,
+      parseInt(request.params.id),
+    );
 
     return results.includes(true);
   }
